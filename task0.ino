67548237d961954b6b2fa782a5c096fbe60e2c42
Hana Fares <103067954+hanafares25@users.noreply.github.com>
2023-05-11 23:45:10 +0300

    mutiple reponse

--- task0.ino
+++ task0.ino
@@ -1,46 +1,57 @@
 #include <Servo.h>
 
+int out1 = 12;
+int out2 = 13;
+int out3 = 14;
 
+int in1 = 4;
+int in2 = 5;
+int in3 = 6;
 
-int out = 12;
-int in = 4;
-
-int rd;
+int rd1, rd2, rd3;
 
 void setup()
-
 {
+  pinMode(out1, OUTPUT);
+  pinMode(out2, OUTPUT);
+  pinMode(out3, OUTPUT);
 
-pinMode(out,OUTPUT);
-
-pinMode(in,INPUT);
-
+  pinMode(in1, INPUT);
+  pinMode(in2, INPUT);
+  pinMode(in3, INPUT);
 }
 
 void loop()
-
 {
+  rd1 = digitalRead(in1);
+  rd2 = digitalRead(in2);
+  rd3 = digitalRead(in3);
 
-rd=digitalRead(in);
+  if (rd1 == HIGH)
+  {
+    digitalWrite(out1, LOW);
+  }
+  else
+  {
+    digitalWrite(out1, HIGH);
+  }
 
-if(rd==HIGH)
+  if (rd2 == HIGH)
+  {
+    digitalWrite(out2, LOW);
+  }
+  else
+  {
+    digitalWrite(out2, HIGH);
+  }
 
-{
-
-digitalWrite(out,LOW);
-
-delay(100);
-
+  if (rd3 == HIGH)
+  {
+    digitalWrite(out3, LOW);
+  }
+  else
+  {
+    digitalWrite(out3, HIGH);
+  }
 }
 
-else
-
-{
-
-digitalWrite(out,HIGH);
-
-delay(100);
-
-}
-
-}
